e522fe493c999875ef99bb0c5886f9213173cf30
crhuber <ch@hellofresh.com>
2019-04-23 17:59:20 +0200

    add new parameter

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ app_config = {
     'DRAIN_WAIT': 60,
     'AUTOSCALER_NAMESPACE': 'kube-system',
     'AUTOSCALER_DEPLOYMENT': 'cluster-autoscaler-aws-cluster-autoscaler',
+    'ASG_STATE_TAG': 'eks-rolling-update:desired_capacity',
     'MAX_RETRY': 5,
     'WAIT': 60,
-    'DRY_RUN': True
+    'DRY_RUN': False
 }
