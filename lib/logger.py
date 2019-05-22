cca2105cedcfe9a84960a645c4634080c21f6a04
crhuber <ch@hellofresh.com>
2019-05-22 15:43:32 +0200

    fix logging

--- lib/logger.py
+++ lib/logger.py
@@ -2,13 +2,13 @@ import logging
 
 # Create a custom logger
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 # Create handlers
 stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
 
 # Create formatters and add it to handlers
-stream_format = '%(asctime)s %(levelname)-8s %(message)s'
+stream_format = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
 stream_handler.setFormatter(stream_format)
 
 # Add handlers to the logger
