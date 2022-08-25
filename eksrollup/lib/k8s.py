786c9399ab448a8e6a57db34984e7547a42e890d
Cristian Doru Serbanoiu <lilo.conf@gmail.com>
2022-08-25 19:17:19 +0200

    [fix] replace deprecated drain option

--- eksrollup/lib/k8s.py
+++ eksrollup/lib/k8s.py
@@ -170,7 +170,7 @@ def drain_node(node_name):
     kubectl_args = [
         'kubectl', 'drain', node_name,
         '--ignore-daemonsets',
-        '--delete-local-data'
+        '--delete-emptydir-data'
     ]
     kubectl_args += app_config['EXTRA_DRAIN_ARGS']
 
