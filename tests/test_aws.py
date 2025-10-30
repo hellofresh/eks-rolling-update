979c8e510d634346f5d0d60d717bd4f11f5ed854
Alex Pogodin <alex.pogodin@ext.hellofresh.com>
2025-10-30 19:28:32 +0200

    fix: create_default_vpc

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -12,8 +12,10 @@ from unittest.mock import patch
 class TestAWS(unittest.TestCase):
 
     def setUp(self):
+        ec2 = boto3.client('ec2')
+        ec2.create_default_vpc()
+        
         client = boto3.client('autoscaling')
-        client.create_default_vpc()
         # create asg
         client.create_launch_configuration(
             LaunchConfigurationName='mock-lc-01',
